af04ae71cd898491bbe483e647617c4bc9dffacd
VarshaShelake <shelakevn@gmail.com>
2022-12-08 12:58:21 +0530

    variable declaration and initialization in js
    variable declaration and initialization in js

---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+
+var myFullName;
+console.log(myFullName);
+myFullName="Varsha Shelake";
+console.log(myFullName);
+myFullName="Varsharani Nana Shelake"
+console.log(myFullName);
+var myLove;
+myLove="I love only JS"
+console.log(myLove);
+var firstName="Varsha"
+var lastName="Shelake"
+var age="26"
+var address="Flat no-06,Saptshrungi Hosing Society,Pandurang Park,Near-Laxmi Jwellers,Ganesh Nagar,Nigdi -"
+var pincode="411062"
+var gender="Female"
+var is_married="Yes"
+console.log(firstName,lastName,age,address,pincode,gender,is_married);
