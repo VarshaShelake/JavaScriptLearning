588e071a210540365120bc8d815ec1c08f60c730
VarshaShelake <shelakevn@gmail.com>
2022-12-09 12:29:11 +0530

    string, number swapping assignment
    string, number swapping assignment

---
+++ 01_swapping_assign.js
@@ -0,0 +1,30 @@
+
+ String, firstString="JavaSript";
+String, secondString="Learning"
+String, thirdString="Guidence"
+var temp;
+console.log('============String Swapping============');
+console.log('Before :',firstString,secondString,thirdString);
+
+temp=firstString;
+firstString=secondString;
+secondString=thirdString;
+thirdString=temp;
+console.log('After :',firstString,secondString,thirdString);
+
+console.log('=============Number Swapping===========');
+var firstNumber
+var secondNumber
+var thirdNumber
+var temp;
+firstNumber=111
+secondNumber=222
+thirdNumber=333
+console.log('Before :', firstNumber,secondNumber,thirdNumber);
+temp=firstNumber
+firstNumber=secondNumber
+secondNumber=thirdNumber
+thirdNumber=temp
+console.log('After :', firstNumber,secondNumber,thirdNumber);
+
+
