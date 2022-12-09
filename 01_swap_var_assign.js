d8a450104ef0b8f40270132893548b7f8e064282
VarshaShelake <shelakevn@gmail.com>
2022-12-09 16:40:52 +0530

    swapping assign
    swapping assign

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,49 @@
+
+ String, firstString="JavaSript";
+String, secondString="Learning"
+String, thirdString="Guidence"
+var temp;
+var sweety="Sweety"
+var cutie="Cutie"
+var temp
+console.log('=======================String Swapping===================');
+console.log("Before : ",sweety,cutie);
+temp=sweety
+sweety=cutie
+cutie=temp
+console.log('After  : ',sweety,cutie);
+console.log('.........................................................');
+console.log('Before :',firstString,secondString,thirdString);
+
+temp=firstString;
+firstString=secondString;
+secondString=thirdString;
+thirdString=temp;
+console.log('After  :',firstString,secondString,thirdString);
+
+console.log('========================Number Swapping==================');
+var firstNumber
+var secondNumber
+var thirdNumber
+var temp;
+var num1=100
+var num2=200
+var num3=300
+console.log("Before :",num1,num2,num3)
+temp=num1
+num1=num2
+num2=num3
+num3=temp
+console.log('After  :',num1,num2,num3);
+console.log('.........................................................');
+firstNumber=111
+secondNumber=222
+thirdNumber=333
+console.log('Before :', firstNumber,secondNumber,thirdNumber);
+temp=firstNumber
+firstNumber=secondNumber
+secondNumber=thirdNumber
+thirdNumber=temp
+console.log('After  :', firstNumber,secondNumber,thirdNumber);
+
+
