ed1ccbade59e446f32536df8d8626e21f8106a9d
VarshaShelake <shelakevn@gmail.com>
2022-12-09 16:39:58 +0530

    typeOf operator
    typeOf operator

---
+++ 01_typeOf.js
@@ -0,0 +1,27 @@
+// var companyName;
+// console.log(companyName);
+// console.log(typeof companyName);
+console.log('====================ypeOf Operator=================');
+console.log('.....................1......................');
+var bankName;
+console.log(bankName);  
+bankName="SBI Bank"
+console.log(bankName,typeof bankName);
+console.log('....................2........................');
+var marks='90%'
+console.log(marks);
+console.log(typeof marks);
+console.log('....................3........................');
+var isWorking
+console.log(isWorking);
+isWorking=true
+console.log(isWorking);
+console.log('.....................3.1.....................');
+console.log(typeof isWorking);
+console.log('.....................4.a.....................');
+var totalCount="hundered and seven"
+console.log(totalCount, typeof totalCount);
+console.log('.....................4.b.....................');
+var totalCount=107
+console.log(totalCount,typeof totalCount);
+
