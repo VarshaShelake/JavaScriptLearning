3ebee11c34bda69101a451925bece545788e8143
VarshaShelake <shelakevn@gmail.com>
2022-12-16 16:27:11 +0530

    comparison operator (equal) compaire without data type
    comparison operator (equal) compaire without data type

---
+++ 04_equalOperator.js
@@ -0,0 +1,16 @@
+console.log(`\n **************** Comparison Operator Hands On ****************`);
+var op1,op2,op3,op4,op5,op6,op7
+op1=0==``
+console.log(`\n The Result of (o=='') is ${op1}`);
+op2=0=='0' 
+console.log(`\n The Result of (0=='0') is ${op2}`);
+op3=0==false
+console.log(`\n The Result of (0=='false') is ${op3}`);
+op4=null==undefined
+console.log(`\n The Result of (null==undefined) is ${op4}`);
+op5=1==[1]
+console.log(`\n The Result of (1==[1]) is ${op5}`);
+op6=1==true
+console.log(`\n The Result of (1==true) is ${op6}`);
+op7=1=='1'
+console.log(`\n The Result of (1=='1) is ${op7}`);
