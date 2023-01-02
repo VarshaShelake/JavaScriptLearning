29ab6947487e6e5d0555d78daae5381143905ff5
VarshaShelake <shelakevn@gmail.com>
2023-01-02 19:57:54 +0530

    obj advance
    obj advance

--- 07_objectAdvance.js
+++ 07_objectAdvance.js
@@ -42,3 +42,17 @@ for (const key in empJay) {
 }
 }
 traversObject(empJay);
+
+function Student(name, rollNo, age){
+    this.name = name;
+    this.rollNo = rollNo;
+    this.age = age;
+ }
+ let s1 = new Student("Vinny", 111, 23);
+ console.log(s1.rollNo);
+ let s2 = new Student("Jenny", 22, 35);
+ console.log(s1);
+
+let isInstance = studChetan instanceof Employee;
+console.log(isInstance);
+// Object is an instance of class
