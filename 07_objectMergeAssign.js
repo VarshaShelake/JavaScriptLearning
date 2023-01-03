751dee14516352fb04c091c154a3e606fb33afd1
VarshaShelake <shelakevn@gmail.com>
2023-01-03 13:41:33 +0530

    object merge assign
    object merge assign

--- 07_objectMergeAssign.js
+++ 07_objectMergeAssign.js
@@ -1,14 +1,19 @@
 const car={
-    name:"Swift",
-    showroom_Price:"6.5L To 9L",
-    color:"Purpole",
-    // company:"Maruti Suzuki",
-    // Seator:"5-Seator",
+    // carName:"Swift",
+    // showroom_Price:"6.5L To 9L",
+    // color:"Purpole",
+    // // company:"Maruti Suzuki",
+    // // Seator:"5-Seator",
+    carName:"Creta SX",
+    company:"Hundai",
+    lounchYear:"2017"
 }
 const carEngin={
-    engine:"1.2 L 4-cylinder",
-    fuel_economy:"23-31km/L",
-    fuel_tank_capacity: "37L To 55L"
+    // engine:"1.2 L 4-cylinder",
+    // fuel_economy:"23-31km/L",
+    // fuel_tank_capacity: "37L To 55L"
+    enginePower:"1199CC",
+    maxPower:"113BHP"
 }
 console.log(`Car Object :`);
 console.log(car);
